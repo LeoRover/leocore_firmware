ffe4182f3d7fd1eb993c0bfdca350c3c39d55276
Błażej Sowa <bsowa123@gmail.com>
2021-10-29 16:05:21 +0200

    Add WheelController

---
+++ Inc/firmware/wheel_controller.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+
+#include "firmware/motor_controller.hpp"
+#include "firmware/pid_regulator.hpp"
+#include "firmware/utils.hpp"
+
+class WheelController {
+ public:
+  WheelController(const MotorConfiguration& motor_conf, bool reverse_polarity);
+
+  /**
+   * @brief Initialize and enable the wheel controller
+   */
+  void init();
+
+  /**
+   * @brief Perform an update routine
+   * @param dt_ms Time elapsed since the last call to update function
+   */
+  void update(uint32_t dt_ms);
+
+  /**
+   * @brief Set the target velocity of the wheel in rad/s
+   */
+  void setSpeed(float speed);
+
+  /**
+   * @brief Get the current velocity of the motor
+   */
+  float getSpeed();
+
+  /**
+   * @brief Get the current power (PWM Duty) applied to the motor
+   */
+  int16_t getPower();
+
+  /**
+   * @brief Get the current distance traversed by the wheel in radians
+   */
+  int32_t getDistance();
+
+  /**
+   * @brief Reset the distance traversed by the wheel
+   */
+  void resetDistance();
+
+  void enable();
+  void disable();
+
+ private:
+  MotorController motor_;
+  PIDRegulator v_reg_;
+  CircularBuffer<std::pair<int32_t, uint32_t>> encoder_buffer_;
+
+  int16_t power_ = 0;
+
+  bool enabled_ = false;
+
+  int32_t ticks_now_ = 0;
+  int32_t ticks_sum_ = 0;
+  uint32_t dt_sum_ = 0;
+
+  float v_target_ = 0.0F;
+  float v_now_ = 0.0F;
+};
---
+++ Src/firmware/wheel_controller.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+
+#include "firmware/configuration.hpp"
+#include "firmware/motor_controller.hpp"
+#include "firmware/parameters.hpp"
+#include "firmware/utils.hpp"
+#include "firmware/wheel_controller.hpp"
+
+static constexpr float PI = 3.141592653F;
+
+WheelController::WheelController(const MotorConfiguration& motor_conf,
+                                 const bool reverse_polarity)
+    : motor_(motor_conf), encoder_buffer_(ENCODER_BUFFER_SIZE) {
+  if (reverse_polarity) {
+    motor_.setMotorPolarity(Polarity::Reversed);
+    motor_.setEncoderPolarity(Polarity::Reversed);
+  }
+}
+
+void WheelController::init() {
+  v_reg_.setCoeffs(params.motor_pid_p, params.motor_pid_i, params.motor_pid_d);
+  v_reg_.setRange(
+      std::min(static_cast<float>(PWM_RANGE), params.motor_power_limit));
+  motor_.init();
+}
+
+void WheelController::update(const uint32_t dt_ms) {
+  int32_t ticks_prev = ticks_now_;
+  ticks_now_ = motor_.getEncoderCnt();
+
+  int32_t new_ticks = ticks_now_ - ticks_prev;
+
+  std::pair<int32_t, uint32_t> encoder_old =
+      encoder_buffer_.push_back(std::pair<int32_t, uint32_t>(new_ticks, dt_ms));
+
+  ticks_sum_ += new_ticks;
+  dt_sum_ += dt_ms;
+
+  ticks_sum_ -= encoder_old.first;
+  dt_sum_ -= encoder_old.second;
+
+  v_now_ = static_cast<float>(ticks_sum_) / (dt_sum_ * 0.001F);
+
+  if (enabled_) {
+    float v_err = v_now_ - v_target_;
+    power_ = v_reg_.update(v_err, dt_ms);
+    motor_.setPower(power_);
+  }
+}
+
+void WheelController::setSpeed(const float speed) {
+  v_target_ = (speed / (2.0F * PI)) * params.motor_encoder_resolution;
+}
+
+float WheelController::getSpeed() {
+  return (v_now_ / params.motor_encoder_resolution) * (2.0F * PI);
+}
+
+int16_t WheelController::getPower() { return power_; }
+
+int32_t WheelController::getDistance() {
+  return (ticks_now_ / params.motor_encoder_resolution) * (2.0F * PI);
+}
+
+void WheelController::resetDistance() {
+  motor_.resetEncoderCnt();
+  ticks_now_ = 0;
+}
+
+void WheelController::enable() {
+  if (!enabled_) {
+    v_reg_.reset();
+    enabled_ = true;
+  }
+}
+
+void WheelController::disable() {
+  enabled_ = false;
+  power_ = 0;
+  motor_.setPower(0);
+}

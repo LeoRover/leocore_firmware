ea2dac93d051fc09667c4aeb6bea88b53b3c0b5f
Błażej Sowa <bsowa123@gmail.com>
2021-11-03 19:26:11 +0100

    Update parameter names

--- Src/firmware/parameters.cpp
+++ Src/firmware/parameters.cpp
@@ -2,31 +2,31 @@
 
 #include "firmware/parameters.hpp"
 
-static constexpr int TIMEOUT = 100;
+static constexpr int TIMEOUT = 1000;
 
 Parameters params;
 
 void Parameters::load(ros::NodeHandle &nh) {
   char *robot_frame_id_ptr = robot_frame_id;
-  nh.getParam("core2/robot_frame_id", &robot_frame_id_ptr, 1, TIMEOUT);
+  nh.getParam("leo_hat/robot_frame_id", &robot_frame_id_ptr, 1, TIMEOUT);
   char *odom_frame_id_ptr = odom_frame_id;
-  nh.getParam("core2/odom_frame_id", &odom_frame_id_ptr, 1, TIMEOUT);
+  nh.getParam("leo_hat/odom_frame_id", &odom_frame_id_ptr, 1, TIMEOUT);
   char *imu_frame_id_ptr = imu_frame_id;
-  nh.getParam("core2/imu_frame_id", &imu_frame_id_ptr, 1, TIMEOUT);
-  char *gps_frame_id_ptr = gps_frame_id;
-  nh.getParam("core2/gps_frame_id", &gps_frame_id_ptr, 1, TIMEOUT);
+  nh.getParam("leo_hat/imu_frame_id", &imu_frame_id_ptr, 1, TIMEOUT);
 
-  nh.getParam("core2/motors/encoder_resolution", &motor_encoder_resolution, 1,
+  nh.getParam("leo_hat/motors/encoder_resolution", &motor_encoder_resolution, 1,
               TIMEOUT);
-  nh.getParam("core2/motors/pid/p", &motor_pid_p, 1, TIMEOUT);
-  nh.getParam("core2/motors/pid/i", &motor_pid_i, 1, TIMEOUT);
-  nh.getParam("core2/motors/pid/d", &motor_pid_d, 1, TIMEOUT);
-  nh.getParam("core2/motors/power_limit", &motor_power_limit, 1, TIMEOUT);
+  nh.getParam("leo_hat/motors/torque_constant", &motor_encoder_resolution, 1,
+              TIMEOUT);
+  nh.getParam("leo_hat/motors/pid/p", &motor_pid_p, 1, TIMEOUT);
+  nh.getParam("leo_hat/motors/pid/i", &motor_pid_i, 1, TIMEOUT);
+  nh.getParam("leo_hat/motors/pid/d", &motor_pid_d, 1, TIMEOUT);
+  nh.getParam("leo_hat/motors/power_limit", &motor_power_limit, 1, TIMEOUT);
 
-  nh.getParam("core2/diff_drive/wheel_radius", &dd_wheel_radius, 1, TIMEOUT);
-  nh.getParam("core2/diff_drive/wheel_separation", &dd_wheel_separation, 1,
+  nh.getParam("leo_hat/diff_drive/wheel_radius", &dd_wheel_radius, 1, TIMEOUT);
+  nh.getParam("leo_hat/diff_drive/wheel_separation", &dd_wheel_separation, 1,
               TIMEOUT);
-  nh.getParam("core2/diff_drive/angular_velocity_multiplier",
+  nh.getParam("leo_hat/diff_drive/angular_velocity_multiplier",
               &dd_angular_velocity_multiplier, 1, TIMEOUT);
-  nh.getParam("core2/diff_drive/input_timeout", &dd_input_timeout, 1, TIMEOUT);
+  nh.getParam("leo_hat/diff_drive/input_timeout", &dd_input_timeout, 1, TIMEOUT);
 }

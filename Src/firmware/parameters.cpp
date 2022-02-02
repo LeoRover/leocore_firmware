e021a1e8ea63e5f8b079663778f9ff53e9bb59b4
Błażej Sowa <bsowa123@gmail.com>
2022-02-02 20:11:37 +0100

    Update parameter names

--- Src/firmware/parameters.cpp
+++ Src/firmware/parameters.cpp
@@ -5,14 +5,14 @@
 static constexpr int TIMEOUT = 1000;
 
 void Parameters::load(ros::NodeHandle &nh) {
-  nh.getParam("firmware/motors/encoder_resolution", &wheel_encoder_resolution, 1,
+  nh.getParam("firmware/wheels/encoder_resolution", &wheel_encoder_resolution, 1,
               TIMEOUT);
-  nh.getParam("firmware/motors/torque_constant", &wheel_torque_constant, 1,
+  nh.getParam("firmware/wheels/torque_constant", &wheel_torque_constant, 1,
               TIMEOUT);
-  nh.getParam("firmware/motors/pid/p", &wheel_pid_p, 1, TIMEOUT);
-  nh.getParam("firmware/motors/pid/i", &wheel_pid_i, 1, TIMEOUT);
-  nh.getParam("firmware/motors/pid/d", &wheel_pid_d, 1, TIMEOUT);
-  nh.getParam("firmware/motors/pwm_duty_limit", &wheel_pwm_duty_limit, 1, TIMEOUT);
+  nh.getParam("firmware/wheels/pid/p", &wheel_pid_p, 1, TIMEOUT);
+  nh.getParam("firmware/wheels/pid/i", &wheel_pid_i, 1, TIMEOUT);
+  nh.getParam("firmware/wheels/pid/d", &wheel_pid_d, 1, TIMEOUT);
+  nh.getParam("firmware/wheels/pwm_duty_limit", &wheel_pwm_duty_limit, 1, TIMEOUT);
 
   nh.getParam("firmware/diff_drive/wheel_radius", &dd_wheel_radius, 1, TIMEOUT);
   nh.getParam("firmware/diff_drive/wheel_separation", &dd_wheel_separation, 1,
